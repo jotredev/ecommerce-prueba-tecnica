4a103477f8b58632eafdcf2ad693c890ea13f571
Jorge Trejo <jorgeetrejoo@gmail.com>
2025-03-19 11:04:13 -0600

    Configuración correcta de las rutas

--- vite.config.ts
+++ vite.config.ts
@@ -8,13 +8,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@components": path.resolve(__dirname, "./src/components"),
-      "@assets": path.resolve(__dirname, "./src/assets"),
-      "@pages": path.resolve(__dirname, "./src/pages"),
-      "@utils": path.resolve(__dirname, "./src/utils"),
-      "@context": path.resolve(__dirname, "./src/context"),
-      "@hooks": path.resolve(__dirname, "./src/hooks"),
-      "@interfaces": path.resolve(__dirname, "./src/interfaces")
+      "@": path.resolve(__dirname, "./src")
     }
   }
 });
